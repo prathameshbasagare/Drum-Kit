65f4d244930936bc194edfe88e615f338916100f
prathameshbasagare5 <119438756+prathameshbasagare5@users.noreply.github.com>
2024-02-21 20:52:32 +0530

    updated the styles and animation

---
+++ index.js
@@ -0,0 +1,66 @@
+var btnList = document.querySelectorAll("button.drum");
+
+for(var i=0;i<btnList.length;i++){
+    btnList[i].addEventListener("click",function (){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    })
+}
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+        // audio.play();
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);    
+    buttonAnimation(event.key);
+
+})
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;tom2
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            console.log(key);
+            break;
+    }
+}
+
+function buttonAnimation(btnKey){
+    // var btnList= document.querySelectorAll("button");
+    // for(var i=0;i<btnList.length;i++){
+    //     btnList[i].classList.remove("pressed");
+        
+    // }
+    document.querySelector("button."+btnKey).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("button."+btnKey).classList.remove("pressed");
+    },100);
+}
